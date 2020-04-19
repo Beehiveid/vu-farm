3f763234afcbd0525a2e7ad5664b105e91e0d380
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2020-04-19 13:55:37 +0700

    create navigation and adjust footer content

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+<v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+        <Navigation />
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+        <v-toolbar-title>Beehive - Farm Workshop</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+        <v-container>
+        </v-container>
+    </v-content>
+
+    <v-footer app>
+        <span>&copy; {{new Date().getFullYear()}}</span>
+    </v-footer>
+</v-app>
+</template>
+
+<script>
+import Navigation from './Navigation'
+export default {
+    components: {
+        Navigation
+    },
+    data: () => ({
+        drawer: true,
+    }),
+    created() {
+        this.$vuetify.theme.dark = true
+    },
+}
+</script>
---
+++ src/components/NavigationItem.vue
@@ -0,0 +1,16 @@
+<template>
+<v-list-item link>
+    <v-list-item-action>
+        <v-icon>{{menu.icon}}</v-icon>
+    </v-list-item-action>
+    <v-list-item-content>
+        <v-list-item-title>{{menu.title}}</v-list-item-title>
+    </v-list-item-content>
+</v-list-item>
+</template>
+
+<script>
+export default {
+    props: ['menu']
+}
+</script>
