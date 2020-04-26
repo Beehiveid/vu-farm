f800ff5c5242c52c6e25b24db50d33d857fcc88d
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2020-04-26 19:10:49 +0700

    add employee CRUD components

--- src/components/view/Employee.vue
+++ src/components/view/Employee.vue
@@ -1,32 +1,152 @@
 <template>
 <v-container>
     <v-row>
-        <employee-card v-for="(item, index) in employees" :key="index" v-bind:employee="item" />
+        <template>
+            <v-data-table :search="search" :headers="headers" :items="employees" class="elevation-1">
+                <template v-slot:top>
+                    <v-toolbar flat color="white">
+                        <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
+                        <v-spacer></v-spacer>
+                        <v-dialog v-model="dialog" max-width="500px">
+                            <template v-slot:activator="{ on }">
+                                <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+                            </template>
+                            <v-card>
+                                <v-card-title>
+                                    <span class="headline">{{ formTitle }}</span>
+                                </v-card-title>
+                                <v-card-text>
+                                    <v-container>
+                                        <v-row>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="editedItem.name" label="Employee Name"></v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="editedItem.title" label="Title"></v-text-field>
+                                            </v-col>
+                                        </v-row>
+                                    </v-container>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                                    <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                    </v-toolbar>
+                </template>
+                <template v-slot:item.actions="{ item }">
+                    <v-icon small class="mr-2" @click="editItem(item)">
+                        mdi-pencil
+                    </v-icon>
+                    <v-icon small @click="deleteItem(item)">
+                        mdi-delete
+                    </v-icon>
+                </template>
+                <template v-slot:no-data>
+                    <v-btn color="primary" @click="initialize">Reset</v-btn>
+                </template>
+            </v-data-table>
+        </template>
     </v-row>
-
 </v-container>
 </template>
 
 <script>
-import EmployeeCard from './EmployeeCard'
 export default {
     components: {
-        EmployeeCard
+
     },
     data: () => ({
-        employees: [{
-                title: "Developer",
-                name: "Nanda"
+        dialog: false,
+        search: '',
+        employees: [],
+        editedIndex: -1,
+        editedItem: {
+            name: '',
+            title: ''
+        },
+        defaultItem: {
+            name: '',
+            title: ''
+        },
+        headers: [{
+                text: 'Name',
+                align: 'start',
+                value: 'name',
             },
             {
-                title: "Developer",
-                name: "Isti Paleman"
+                text: 'Title',
+                value: 'title'
             },
             {
-                title: "Big Bossu",
-                name: "Cipal"
+                text: 'Actions',
+                value: 'actions',
+                sortable: false
             },
         ]
-    })
+    }),
+    computed: {
+        formTitle() {
+            return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        },
+    },
+
+    watch: {
+        dialog(val) {
+            val || this.close()
+        },
+    },
+
+    created() {
+        this.initialize()
+    },
+
+    methods: {
+        initialize() {
+            this.employees = [{
+                    title: "Developer",
+                    name: "Nanda"
+                },
+                {
+                    title: "Developer",
+                    name: "Isti Paleman"
+                },
+                {
+                    title: "Big Bossu",
+                    name: "Cipal"
+                },
+            ]
+        },
+
+        editItem(item) {
+            this.editedIndex = this.employees.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialog = true
+        },
+
+        deleteItem(item) {
+            const index = this.employees.indexOf(item)
+            confirm('Are you sure you want to delete this employee?') && this.employees.splice(index, 1)
+        },
+
+        close() {
+            this.dialog = false
+            setTimeout(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            }, 300)
+        },
+
+        save() {
+            if (this.editedIndex > -1) {
+                Object.assign(this.employees[this.editedIndex], this.editedItem)
+            } else {
+                this.employees.push(this.editedItem)
+            }
+            this.close()
+        },
+    },
 }
 </script>
