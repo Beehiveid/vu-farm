9ce76182a924b4ebbf348f5cdce1e651dca5a0f1
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2020-04-27 21:33:10 +0700

    remove reset button when no data

--- src/components/view/Employee.vue
+++ src/components/view/Employee.vue
@@ -44,9 +44,6 @@
                         mdi-delete
                     </v-icon>
                 </template>
-                <template v-slot:no-data>
-                    <v-btn color="primary" @click="initialize">Reset</v-btn>
-                </template>
             </v-data-table>
         </template>
     </v-row>
